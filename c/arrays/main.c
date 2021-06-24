6b001f61c99d9589a3999202c31c7c6cf2f94f8c
XmchxCoder <1394466835@qq.com>
2021-06-24 10:02:53 +0800

    实现C版Vector

---
+++ c/arrays/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include "vector.h"
+#include "vector.c"
+
+
+int main() {
+    XArray *arrayptr = xarray_new(1);
+
+    assert(xarray_size(arrayptr) == 0);
+    assert(xarray_capacity(arrayptr) == kDefaultCapacity);
+    assert(xarray_is_empty(arrayptr) == true);
+
+    xarray_push(arrayptr, 1);
+    xarray_push(arrayptr, 2);
+    xarray_push(arrayptr, 3);
+    // 1 2 3
+
+    assert(xarray_at(arrayptr, 0) == 1);
+    assert(xarray_at(arrayptr, 2) == 3);
+
+    xarray_insert(arrayptr, 0, 0);
+    xarray_insert(arrayptr, 2, 4);
+    // 0 1 4 2 3
+
+    assert(xarray_at(arrayptr, 2) == 4);
+    assert(xarray_at(arrayptr, 4) == 3);
+
+
+    xarray_prepend(arrayptr, 10);
+    xarray_prepend(arrayptr, 10);
+    xarray_prepend(arrayptr, 10);
+    xarray_prepend(arrayptr, 10);
+    xarray_remove(arrayptr, 10);
+    xarray_delete(arrayptr, 0);
+    assert(xarray_pop(arrayptr) == 3);
+    assert(xarray_find(arrayptr, 4) == 1);
+    assert(xarray_find(arrayptr, 1) == 0);
+    assert(xarray_find(arrayptr, 2) == 2);
+    assert(xarray_find(arrayptr, 10) == -1);
+    assert(xarray_find(arrayptr, 3) == -1);
+    // 1 4 2
+
+    for (int i = 10; i < 30; ++i) {
+        xarray_prepend(arrayptr, i);
+    }
+
+    for (int i = 10; i < 30; ++i) {
+        xarray_remove(arrayptr, i);
+    }
+
+
+    assert(xarray_pop(arrayptr)==2);
+    assert(xarray_pop(arrayptr)==4);
+    assert(xarray_pop(arrayptr)==1);
+
+    xarray_print(arrayptr);
+    xarray_destroy(arrayptr);
+    return 0;
+}
---
+++ c/arrays/vector.h
@@ -0,0 +1,57 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <assert.h>
+#include <stdbool.h>
+
+typedef struct _xm_array {
+    int size;
+    int capacity;
+    int *data;
+} XArray;
+
+const int kDefaultCapacity = 16;
+const int kGrowthFactor = 2;
+const int kShrinkFactor = 4;
+
+// array function
+XArray *xarray_new(int capacity);
+
+void xarray_destroy(XArray *arrayptr);
+
+void xarray_print(XArray *arrayptr);
+
+int xarray_size(XArray *arrayptr);
+
+int xarray_capacity(XArray *arrayptr);
+
+bool xarray_is_empty(XArray *arrayptr);
+
+int xarray_at(XArray *arrayptr, int index);
+
+void xarray_push(XArray *arrayptr, int value);
+
+void xarray_insert(XArray *arrayptr, int index, int value);
+
+void xarray_prepend(XArray *arrayptr, int value);
+
+int xarray_pop(XArray *arrayptr);
+
+void xarray_delete(XArray *arrayptr, int index);
+
+// looks for value and removes index holding it (even if in multiple places)
+void xarray_remove(XArray *arrayptr, int value);
+
+int xarray_find(XArray *arrayptr, int value);
+
+
+// tool function
+void check_address(void *p);
+
+void check_index(XArray *arrayptr, int index);
+
+void check_insert_index(XArray *arrayptr, int index);
+
+void resize(XArray *arrayptr, int new_capacity);
+
+#endif
